9aac2a94d05444681b4cbe4fa43bbe2670208e28
Joe Rabinoff <jrabinoff6@math.gatech.edu>
2020-04-29 09:14:31 -0400

    Added GPL3 licensing

--- linalg/test/polynomial_test.js
+++ linalg/test/polynomial_test.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2020 Joseph Rabinoff
+ * All rights reserved
+ *
+ * This file is part of linalg.js.
+ *
+ * linalg.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linalg.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linalg.js.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 'use strict'; // -*- js2 -*-
 
 import './lib/resemble.js';
