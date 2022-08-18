7583bdd052eff8e76d9ea4d80ef10c88b4f3fe73
Joe Rabinoff <rabinoff@post.harvard.edu>
2022-08-18 09:29:13 -0400

    Deleted linalg.js

--- linalg/.mocharc.js
+++
@@ -1,4 +0,0 @@
-const { colors } = require('mocha/lib/reporters/base');
-// The default color is the background color on my terminal.
-colors.pass = 32;
-
--- linalg/project-settings.el
+++
@@ -1,5 +0,0 @@
-
-(setq-local projectile-project-compilation-cmd "tsc"
-            projectile-project-compilation-dir "."
-            projectile-project-test-cmd "npm test"
-            compilation-read-command nil)
